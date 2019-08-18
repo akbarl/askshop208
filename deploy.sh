78a56845dd5166f5c0b84d9ff9d3183728a2bb0f
akbarl <akbarl@live.com>
2019-08-18 17:24:21 +0700

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "Build askshop208 image"
+IMAGE_NAME = "askshop208"
+EXTERNAL_PORT = 8181
+INTERNAL_PORT = 8181
+echo "Building $IMAGE_NAME image"
+
+sudo docker build -t $IMAGE_NAME .
+
+
