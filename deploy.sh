7881b4294779bcad75fe6d33e5fa564b85c79c6a
Ubuntu <ubuntu@ip-172-31-25-207.us-west-2.compute.internal>
2019-08-18 10:41:42 +0000

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-IMAGE_NAME = "askshop208"
-EXTERNAL_PORT = 8181
-INTERNAL_PORT = 8181
+IMAGE_NAME="askshop208"
+EXTERNAL_PORT=8181
+INTERNAL_PORT=8181
 echo "Building $IMAGE_NAME image"
 
 sudo docker build -t $IMAGE_NAME .
 
+echo "Created $IMAGE_NAME successfully"
 
+echo "Starting a container with $IMAGE_NAME"
+sudo docker run -d -p $EXTERNAL_PORT:$INTERNAL_PORT $IMAGE_NAME
+
+sudo docker ps
+echo "Started the container  successfully"
