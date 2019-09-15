1032e96b9afd00f6398ee4161ccbb3bd1eb97bf9
akbarl <akbarl@live.com>
2019-09-15 22:26:38 +0700

    implementing lazada client and move all lazada handler to lazada service

--- cmd/lazada_service/app.go
+++ cmd/lazada_service/app.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/akbarl/askshop208/internal/service/lazada_service/lazada_handlers"
+import (
+	"net/http"
+
+	"github.com/akbarl/askshop208/internal/service/lazada_service/lazada_handlers"
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	lazada_handlers.NewAuthorizationHandler()
+	r := mux.NewRouter()
+	//lazada_handlers.NewAuthorizationHandler()
+	r.HandleFunc("/auth", lazada_handlers.AuthenticationHandler)
+	r.HandleFunc("/products", lazada_handlers.ProductsHandler)
+	http.ListenAndServe(":8181", r)
 }
--- internal/service/lazada_service/lazada_handlers/authorization_handler.go
+++ internal/service/lazada_service/lazada_handlers/authorization_handler.go
@@ -3,58 +3,37 @@ package lazada_handlers
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 	"time"
 
-	"github.com/akbarl/askshop208/internal/service/lazada_service/lazada_models"
-	"github.com/akbarl/askshop208/pkg/utils/http_utils"
-	"github.com/akbarl/askshop208/pkg/utils/lazada_utils/lazada_constants"
+	"github.com/akbarl/askshop208/pkg/utils/lazada_client"
 	"github.com/akbarl/askshop208/pkg/utils/lazada_utils/lazada_utils"
-	"github.com/gorilla/mux"
 	"github.com/patrickmn/go-cache"
 )
 
 var c = cache.New(5*time.Minute, 10*time.Minute)
 
-func NewAuthorizationHandler() {
-	r := mux.NewRouter()
-	r.HandleFunc("/", IndexHandler)
-	r.HandleFunc("/auth", AuthenticationHandler)
-	http.ListenAndServe(":8181", r)
-}
-
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+type AuthorizationResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	ExpriesIn    int64  `json:"expires_in"`
+	Account      string `json:"account"`
 }
 
 func AuthenticationHandler(w http.ResponseWriter, r *http.Request) {
-	var authInfo lazada_models.AuthorizationResponse
-	item, found := c.Get("authorization_info")
-	if found {
-		authInfo = item.(lazada_models.AuthorizationResponse)
-		log.Println(authInfo.AccessToken)
-		return
-	}
-
+	appKey := lazada_utils.GetAppKey()
+	appSecret := lazada_utils.GetAppSecret()
 	code := r.URL.Query().Get("code")
-	param := lazada_utils.GenerateCommonRequestParameters()
-	param[lazada_constants.CODE] = code
-	param[lazada_constants.SIGN] = lazada_utils.GenerateSignatureRequest(lazada_constants.GENERATE_ACCESS_TOKEN_REST_URL, param)
-	queryString := http_utils.BuildQueryString(param)
-	fullPath := lazada_constants.BASE_AUTH_REST_URL + lazada_constants.GENERATE_ACCESS_TOKEN_REST_URL + queryString
-	log.Println(fullPath)
-	bodyText := []byte(http_utils.ExecuteGetMethod(fullPath))
-	var authResp lazada_models.AuthorizationResponse
-	err := json.Unmarshal(bodyText, &authResp)
-	var emptyAuthResp lazada_models.AuthorizationResponse = lazada_models.AuthorizationResponse{}
-	if err != nil || emptyAuthResp == authResp {
-		log.Println("Invalid authorization code: ", err)
+	lazadaClient := lazada_client.NewLazadaClient(appKey, appSecret)
+	data := []byte(lazadaClient.GetAccessToken(code))
+	var authInfo AuthorizationResponse
+	error := json.Unmarshal(data, &authInfo)
+	if error != nil {
+		fmt.Println("Could not find access token")
 		return
 	}
 
-	c.Add("authorization_info", authResp, cache.NoExpiration)
-
+	if authInfo.AccessToken != "" {
+		c.Set("AuthInfo", authInfo, cache.NoExpiration)
+	}
 }
