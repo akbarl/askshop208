39305e5486c7502bfafdbe4be8c28046edfe0595
akbarl <akbarl@live.com>
2019-09-15 23:25:54 +0700

    update dockerfile and deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,9 @@
 IMAGE_NAME="askshop208"
 EXTERNAL_PORT=8181
 INTERNAL_PORT=8181
+APP_KEY=$APP_KEY
+APP_SECRET=$APP_SECRET
+
 echo "Building $IMAGE_NAME image"
 
 sudo docker build -f cmd/lazada_service/Dockerfile -t $IMAGE_NAME .
@@ -9,7 +12,7 @@ sudo docker build -f cmd/lazada_service/Dockerfile -t $IMAGE_NAME .
 echo "Created $IMAGE_NAME successfully"
 
 echo "Starting a container with $IMAGE_NAME"
-sudo docker run -d -p $EXTERNAL_PORT:$INTERNAL_PORT $IMAGE_NAME
+sudo docker run -d -e APP_KEY=$APP_KEY -e APP_SECRET=$APP_SECRET -p $EXTERNAL_PORT:$INTERNAL_PORT $IMAGE_NAME
 
 sudo docker ps
 echo "Started the container  successfully"
