455e48043f9cea9c010f61da717ac3062e0fbecb
akbarl <akbarl@live.com>
2019-12-01 12:00:47 +0700

    re-structure project

--- internal/service/lazada_service/lazada_models/authorization_response.go
+++
@@ -1,8 +0,0 @@
-package lazada_models
-
-type AuthorizationResponse struct {
-	AccessToken  string `json:"access_token"`
-	RefreshToken string `json:"refresh_token"`
-	ExpriesIn    int64  `json:"expires_in"`
-	Account      string `json:"account"`
-}
