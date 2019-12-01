0cd64c089550ed299756aaeabf5f3f4c2140e7ba
akbarl <akbarl@live.com>
2019-12-01 13:55:17 +0700

    init frontend

---
+++ frontend/src/components/homepage/homepage.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './homepage.css'
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                Hello my friend
+            </div>
+        );
+    }
+}
+
+export default HomePage;
